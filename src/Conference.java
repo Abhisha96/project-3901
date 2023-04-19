b59a53afba246db07672f9eb519ab5c0d5d9da81
athaker <ab570421@dal.ca>
2023-04-19 20:58:38 -0300

    Enlists the attributes of conference

---
+++ src/Conference.java
@@ -0,0 +1,22 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Conference extends PublicationLibrary {
+
+    String uniqueIdentifier;
+    Set<String> authors = new HashSet<>();
+    String paperTitle;
+    String conferenceName;
+    String location;
+    String year;
+    String pageRange;
+
+    void getPublicationVenue(){
+
+    }
+
+    void addConference(){
+
+    }
+
+}
